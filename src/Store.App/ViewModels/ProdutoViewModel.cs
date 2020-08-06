0409a85a8f9a3e4929b6ff55587a7ec76d3b35cc
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-08-06 19:30:23 -0300

    Customização das Views - Fornecedor / Produto CRUD

--- src/Store.App/ViewModels/ProdutoViewModel.cs
+++ src/Store.App/ViewModels/ProdutoViewModel.cs
@@ -21,6 +21,7 @@ namespace Store.App.ViewModels
         [DisplayName("Descrição")]
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
         public string Imagem { get; set; }
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
