8a62dc6ad5fcf641648f4df07b0fdf09a8d7315f
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-08-10 22:43:00 -0300

    Alteração e customização das Views de Fornecedores e Produtos, e criação de partial views para reutilização de código.

---
+++ src/Store.App/Extensions/RazorExtentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.App.Extensions
+{
+    public static class RazorExtentions
+    {
+        public static string FormatarDocumento(this RazorPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/000\-00");
+        }
+    }
+}
