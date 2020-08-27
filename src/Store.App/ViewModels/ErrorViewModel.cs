9a7f25a127b44b38a966f1ff4ecccbc6e26ac73b
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-08-27 18:32:45 -0300

    Tratamento de erros customizados no environment de produção

--- src/Store.App/ViewModels/ErrorViewModel.cs
+++ src/Store.App/ViewModels/ErrorViewModel.cs
@@ -4,8 +4,9 @@ namespace Store.App.ViewModels
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public int ErroCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
